e9bbf84c856c5d346fe5b173cb634a9d88c39823
Rumi152 <pawel.kajdewicz@outlook.com>
2025-11-27 22:02:59 +0100

    Finish simple footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <>
-      <footer className="h-20 w-full bg-amber-700">
-        Header
+      <footer className="h-20 w-full bg-amber-700 flex justify-evenly items-center">
+        <span className="text-amber-100">ARCHERY STORE</span>
+        <span className="text-amber-100">+48 123 456 789</span>
+        <span className="text-amber-100">example.email@example.com</span>
       </footer>
     </>
   );
